f81069ba867da588443f781c90fb76e6c3885b53
Julian Beldotti <julian.beldotti@gmail.com>
2023-06-10 11:54:25 -0500

    Feat: Add MVC flow for a new coupon form

--- app/controllers/merchants/coupons_controller.rb
+++ app/controllers/merchants/coupons_controller.rb
@@ -4,6 +4,25 @@ class Merchants::CouponsController < ApplicationController
   end
   
   def show
-    
+
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = merchant.coupons.new(coupon_params)
+    if coupon.save
+      redirect_to merchant_coupons_path(merchant)
+    else
+      flash[:alert] = "ERROR: Code Not Unique"
+    end
+  end
+
+    private
+    def coupon_params
+      params.permit(:name, :unique_code, :disc_amount, :disc_type, :status)
+    end
 end
