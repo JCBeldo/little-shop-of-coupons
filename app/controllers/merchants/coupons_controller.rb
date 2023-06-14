09794685167f59e654fcbeac479b9d0c67d5709f
Julian Beldotti <julian.beldotti@gmail.com>
2023-06-13 23:41:09 -0500

    Feat: Add MCV flow to limit active coupons in view and remove activate button when maxed, plus add sad path

--- app/controllers/merchants/coupons_controller.rb
+++ app/controllers/merchants/coupons_controller.rb
@@ -1,6 +1,7 @@
 class Merchants::CouponsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @holidays = HolidaySearch.new.holidaze(3)
   end
   
   def show
@@ -15,11 +16,14 @@ class Merchants::CouponsController < ApplicationController
   def create
     merchant = Merchant.find(params[:merchant_id])
     coupon = merchant.coupons.new(coupon_params)
-    if coupon.save
+    if !merchant.max_coupons
+      redirect_to new_merchant_coupon_path(merchant)
+      flash[:alert] = "ERROR: Only 5 active coupons allowed, please deactive one to create a new one."
+    elsif coupon.save 
       redirect_to merchant_coupons_path(merchant)
     else
-      flash[:alert] = "ERROR: Code Not Unique"
       redirect_to new_merchant_coupon_path(merchant)
+      flash[:alert] = "ERROR: Code Not Unique"
     end
   end
 
